8ceaffabeb8797947c56d00211097efc6753d03f
Tadeusz Rymarz <tdrym.dev@pm.me>
2026-04-01 18:45:49 +0200

    Incorporate some changes from outdated tr's pgd-dem branch

---
+++ tutorials/PGF_DEM_test/Allclean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf processor* 0 0.[1-9]* [1-9]* log log.analyzed postProcessing constant/polyMesh dynamicCode
+rm -r proc* spheres springs log*
---
+++ tutorials/PGF_DEM_test/run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+blockMesh
+#createPatch -overwrite | tee log_createPatch
+cp -r 0.orig 0
+#setFields | tee log_setFields
+decomposePar
+mkdir spheres
+
+
+mpirun --allow-run-as-root -n 2 /home/zhiwar/yade-install/bin/yade-Unknown MPI_lambda.py | tee log.txt
+
+
+
+
